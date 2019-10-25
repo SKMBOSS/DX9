da2412ecfbf11ec1b37dfb2f264b46b9f275a97a
SKMBOSS <zzangjungsu@gmail.com>
2019-10-25 16:00:00 +0900

    12 프러스텀컬링 13 쿼드트리

--- 12_FrustumCulling/12_main.cpp
+++ 12_FrustumCulling/12_main.cpp
@@ -2,11 +2,11 @@
 #include <d3dx9.h>
 #include "ZFLog.h"
 #include "ZCamera.h"
+#include "ZFrustum.h"
 #include "SAFE_DELETE.h"
 
 #pragma comment(lib , "d3d9.lib")
 #pragma comment(lib , "d3dx9.lib")
-#pragma comment(lib , "winmm.lib")
 
 #define MAINBODY
 
@@ -19,19 +19,24 @@ HWND						g_hWnd = NULL;
 LPDIRECT3D9					g_pD3D = NULL;
 LPDIRECT3DDEVICE9			g_pd3dDevice = NULL;
 LPDIRECT3DVERTEXBUFFER9		g_pVB = NULL;
-LPDIRECT3DVERTEXBUFFER9		g_pVB2 = NULL;
 LPDIRECT3DINDEXBUFFER9		g_pIB = NULL;
-LPDIRECT3DINDEXBUFFER9		g_pIB2 = NULL;
 LPDIRECT3DTEXTURE9			g_pTexHeight = NULL;
 LPDIRECT3DTEXTURE9			g_pTexDiffuse = NULL;
-LPDIRECT3DTEXTURE9			g_pTexDiffuse2 = NULL;
 D3DXMATRIXA16				g_matAni;
-D3DXMATRIXA16				g_matAni2;
+
+D3DXMATRIXA16				g_matWorld;
+D3DXMATRIXA16				g_matView;
+D3DXMATRIXA16				g_matProj;
+
 DWORD						g_cxHeight = 0;
 DWORD						g_czHeight = 0;
 DWORD						g_dwMouseX = 0;
 DWORD						g_dwMouseY = 0;
-
+int							g_nTriangles = 0;
+D3DXVECTOR3*				g_pvHeightMap = NULL;
+BOOL						g_bHideFrustum = TRUE;
+BOOL						g_bLockFrustum = FALSE;
+BOOL						g_bWireframe = FALSE;
 
 struct CUSTOMVERTEX
 {
@@ -40,14 +45,7 @@ struct CUSTOMVERTEX
 	D3DXVECTOR3	t;
 };
 
-struct CUSTOMVERTEX2
-{
-	FLOAT x, y, z;
-	FLOAT u, v;
-};
-
-#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ|D3DFVF_NORMAL|D3DFVF_TEX1)
-#define D3DFVF_CUSTOMVERTEX2 (D3DFVF_XYZ | D3DFVF_TEX1)
+#define D3DFVF_CUSTOMVVERTEX (D3DFVF_XYZ|D3DFVF_NORMAL|D3DFVF_TEX1)
 
 struct MYINDEX
 {
@@ -77,17 +75,33 @@ HRESULT InitD3D(HWND hWnd)
 	return S_OK;
 }
 
+void InitMatrix()
+{
+	D3DXMatrixIdentity(&g_matWorld);
+	g_pd3dDevice->SetTransform(D3DTS_WORLD, &g_matWorld);
+
+	D3DXVECTOR3 vEyePt(0.0f, 100.0f, -(float)g_czHeight);
+	D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 vUpVec(0.0f, 1.0f, 0.0f);
+	D3DXMatrixLookAtLH(&g_matView, &vEyePt, &vLookatPt, &vUpVec);
+	g_pd3dDevice->SetTransform(D3DTS_VIEW, &g_matView);
+
+	D3DXMatrixPerspectiveFovLH(&g_matProj, D3DX_PI / 4, 1.0f, 1.0f, 1000.0f);
+	g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &g_matProj);
+
+	D3DXMatrixPerspectiveFovLH(&g_matProj, D3DX_PI / 4, 1.0f, 1.0f, 200.0f);
+
+	ZCamera::GetInstance()->SetView(&vEyePt, &vLookatPt, &vUpVec);
+}
+
 HRESULT InitTexture()
 {
 	if (FAILED(D3DXCreateTextureFromFileEx(g_pd3dDevice, BMP_HEIGHTMAP, D3DX_DEFAULT, D3DX_DEFAULT,
 		D3DX_DEFAULT, 0, D3DFMT_X8R8G8B8, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, 0, NULL, NULL,
-		&g_pTexHeight))) //D3DFMT_X8R8G8B8 이거설명 중 jpg png(손실없는압축?)
+		&g_pTexHeight)))
 		return E_FAIL;
 
 	if (FAILED(D3DXCreateTextureFromFile(g_pd3dDevice, "tile2.tga", &g_pTexDiffuse)))
-		return E_FAIL;
-
-	if (FAILED(D3DXCreateTextureFromFile(g_pd3dDevice, "test.bmp", &g_pTexDiffuse2)))
 		return E_FAIL;
 
 	return S_OK;
@@ -95,33 +109,17 @@ HRESULT InitTexture()
 
 HRESULT InitVB()
 {
-	CUSTOMVERTEX2 vertices[] =
-	{
-		{ -5, 50,0, 0,0,},
-		{  5, 50,0, 1,0, },
-		{ -5,20,0, 0,1, },
-		{  5,20,0, 1,1, }
-	};
-
-	if (FAILED(g_pd3dDevice->CreateVertexBuffer(4 * sizeof(CUSTOMVERTEX2), 0, D3DFVF_CUSTOMVERTEX2, D3DPOOL_DEFAULT,
-		&g_pVB2, NULL)))
-		return E_FAIL;
-
-	VOID* pVertices2;
-	if (FAILED(g_pVB2->Lock(0, sizeof(vertices), (void**)&pVertices2, 0)))
-		return E_FAIL;
-	memcpy(pVertices2, vertices, sizeof(vertices));
-	g_pVB2->Unlock();
-
 	D3DSURFACE_DESC ddsd;
 	D3DLOCKED_RECT	d3drc;
 
-	g_pTexHeight->GetLevelDesc(0, &ddsd); //GetLevelDesc Desc가 들어가는건 이옵션에 대한 설명을 기술함->이함수는 실제로 디바이스의 정보를 알 수 있음 
+	g_pTexHeight->GetLevelDesc(0, &ddsd);
 	g_cxHeight = ddsd.Width;
 	g_czHeight = ddsd.Height;
 	ZFLog::GetInstance()->Log("Texturel Size:[%d,%d]", g_cxHeight, g_czHeight);
+	g_pvHeightMap = new D3DXVECTOR3[g_cxHeight * g_czHeight];
+
 	if (FAILED(g_pd3dDevice->CreateVertexBuffer(ddsd.Width*ddsd.Height * sizeof(CUSTOMVERTEX), 0,
-		D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_pVB, NULL)))
+		D3DFVF_CUSTOMVVERTEX, D3DPOOL_DEFAULT, &g_pVB, NULL)))
 		return E_FAIL;
 
 	g_pTexHeight->LockRect(0, &d3drc, NULL, D3DLOCK_READONLY);
@@ -130,7 +128,6 @@ HRESULT InitVB()
 		return E_FAIL;
 
 	CUSTOMVERTEX v;
-
 	CUSTOMVERTEX* pV = (CUSTOMVERTEX*)pVertices;
 	for (DWORD z = 0; z < g_czHeight; z++)
 	{
@@ -138,7 +135,7 @@ HRESULT InitVB()
 		{
 			v.p.x = (float)x - g_cxHeight / 2.0f;
 			v.p.z = -((float)z - g_czHeight / 2.0f);
-			v.p.y = ((float)(*((LPDWORD)d3drc.pBits + x + z * (d3drc.Pitch / 4)) & 0x0000ff)) / 10.0f; // Pitch 로 접근해야함 실제로 데이터적으로 정의된값 height넣으면 큰일남
+			v.p.y = ((float)(*((LPDWORD)d3drc.pBits + x + z * (d3drc.Pitch / 4)) & 0x0000ff)) / 10.0f;
 			v.n.x = v.p.x;
 			v.n.y = v.p.y;
 			v.n.z = v.p.z;
@@ -146,38 +143,18 @@ HRESULT InitVB()
 			v.t.x = (float)x / (g_cxHeight - 1);
 			v.t.y = (float)z / (g_czHeight - 1);
 			*pV++ = v;
+			g_pvHeightMap[z*g_cxHeight + x] = v.p;
 			//g_pLog->Log("[%f ,%f ,%f]", v.p.x, v.p.y, v.p.z);
 		}
 	}
 
 	g_pVB->Unlock();
 	g_pTexHeight->UnlockRect(0);
-
 	return S_OK;
 }
 
 HRESULT InitIB()
 {
-
-	MYINDEX indeices[] =
-	{
-		{3,2,6},{3,6,7},//앞면
-	};
-
-	if (FAILED(g_pd3dDevice->CreateIndexBuffer(2 * sizeof(MYINDEX), 0,
-		D3DFMT_INDEX16, D3DPOOL_DEFAULT, &g_pIB2, NULL)))
-	{
-		return E_FAIL;
-	}
-
-
-	void* pIndices;
-	if (FAILED(g_pIB2->Lock(0, sizeof(indeices), (void**)&pIndices, 0)))
-		return E_FAIL;
-	memcpy(pIndices, indeices, sizeof(indeices));
-	g_pIB2->Unlock();
-
-
 	if (FAILED(g_pd3dDevice->CreateIndexBuffer((g_cxHeight - 1) * (g_czHeight - 1) * 2 * sizeof(MYINDEX), 0,
 		D3DFMT_INDEX16, D3DPOOL_DEFAULT, &g_pIB, NULL)))
 		return E_FAIL;
@@ -207,27 +184,6 @@ HRESULT InitIB()
 	return S_OK;
 }
 
-void SetupCamera()
-{
-	D3DXMATRIXA16 matWorld;
-	D3DXMatrixIdentity(&matWorld);
-	g_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
-
-	D3DXVECTOR3 vEyePt(0.0f, 100.0f, -(float)g_czHeight);
-	D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);
-	D3DXVECTOR3 vUpVec(0.0f, 1.0f, 0.0f);
-	D3DXMATRIXA16 matView;
-	D3DXMatrixLookAtLH(&matView, &vEyePt, &vLookatPt, &vUpVec);
-	g_pd3dDevice->SetTransform(D3DTS_VIEW, &matView);
-
-	D3DXMATRIXA16 matProj;
-	D3DXMatrixPerspectiveFovLH(&matProj, D3DX_PI / 4, 1.0f, 1.0f, 1000.0f);
-	g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &matProj);
-
-	ZCamera::GetInstance()->SetView(&vEyePt, &vLookatPt, &vUpVec);
-}
-
-
 HRESULT InitGeometry()
 {
 	if (FAILED(InitTexture()))
@@ -239,8 +195,8 @@ HRESULT InitGeometry()
 	if (FAILED(InitIB()))
 		return E_FAIL;
 
-	SetupCamera();
-	// 최초의 마우스 위치 보관
+	InitMatrix();
+
 	POINT	pt;
 	GetCursorPos(&pt);
 	g_dwMouseX = pt.x;
@@ -248,6 +204,111 @@ HRESULT InitGeometry()
 
 	return S_OK;
 }
+
+
+VOID SetupLights()
+{
+	D3DMATERIAL9 mtrl;
+	ZeroMemory(&mtrl, sizeof(D3DMATERIAL9));
+	mtrl.Diffuse.r = mtrl.Ambient.r = 1.0f;
+	mtrl.Diffuse.g = mtrl.Ambient.g = 1.0f;
+	mtrl.Diffuse.b = mtrl.Ambient.b = 1.0f;
+	mtrl.Diffuse.a = mtrl.Ambient.a = 1.0f;
+	g_pd3dDevice->SetMaterial(&mtrl);
+
+	D3DXVECTOR3 vecDir;
+	D3DLIGHT9 light;
+	ZeroMemory(&light, sizeof(D3DLIGHT9));
+	light.Type = D3DLIGHT_DIRECTIONAL;
+	light.Diffuse.r = 1.0f;
+	light.Diffuse.g = 1.0f;
+	light.Diffuse.b = 0.0f;
+	//vecDir = D3DXVECTOR3(1, 1, 1);
+	vecDir = D3DXVECTOR3(cosf(GetTickCount() / 350.0f), 1.0f, sinf(GetTickCount() / 350.0f));
+	D3DXVec3Normalize((D3DXVECTOR3*)&light.Direction, &vecDir);
+	light.Range = 1000.0f;
+
+	g_pd3dDevice->SetLight(0, &light);
+	g_pd3dDevice->LightEnable(0, TRUE);
+	g_pd3dDevice->SetRenderState(D3DRS_LIGHTING, TRUE);
+	g_pd3dDevice->SetRenderState(D3DRS_AMBIENT, 0x00909090);
+}
+
+void LogStatus()
+{
+	ZFLog::GetInstance()->Log("Wireframe:%d", g_bWireframe);
+	ZFLog::GetInstance()->Log("HideFrustum:%d", g_bHideFrustum);
+	ZFLog::GetInstance()->Log("LockFrustum:%d", g_bLockFrustum);
+}
+
+void LogFPS(void)
+{
+	static DWORD nTick = 0;
+	static DWORD nFPS = 0;
+
+	if (GetTickCount() - nTick > 1000)
+	{
+		nTick = GetTickCount();
+		ZFLog::GetInstance()->Log("FPS : %d", nFPS);
+		nFPS = 0;
+		LogStatus();
+		return;
+	}
+
+	nFPS++;
+}
+
+HRESULT ProcessFrustumCull()
+{
+	DWORD		i[4];
+	BOOL		b[4];
+	MYINDEX		idx;
+	MYINDEX*	pI;
+
+	if (FAILED(g_pIB->Lock(0, (g_cxHeight - 1) * (g_cxHeight - 1) * 2 * sizeof(MYINDEX), (void**)&pI, 0)))
+		return E_FAIL;
+
+	g_nTriangles = 0;
+
+	for (DWORD z = 0; z < g_czHeight - 1; z++)
+	{
+		for (DWORD x = 0; x < g_cxHeight - 1; x++)
+		{
+			i[0] = (z*g_cxHeight + x);
+			i[1] = (z*g_cxHeight + x + 1);
+			i[2] = ((z + 1)*g_cxHeight + x);
+			i[3] = ((z + 1)*g_cxHeight + x + 1);
+
+			b[0] = ZFrustum::GetInstance()->IsIn(&g_pvHeightMap[i[0]]);
+			b[1] = ZFrustum::GetInstance()->IsIn(&g_pvHeightMap[i[1]]);
+			b[2] = ZFrustum::GetInstance()->IsIn(&g_pvHeightMap[i[2]]);
+			if (b[0] | b[1] | b[2])
+			{
+				idx._0 = i[0];
+				idx._1 = i[1];
+				idx._2 = i[2];
+				*pI++ = idx;
+				g_nTriangles++;
+			}
+
+			b[2] = ZFrustum::GetInstance()->IsIn(&g_pvHeightMap[i[2]]);
+			b[1] = ZFrustum::GetInstance()->IsIn(&g_pvHeightMap[i[1]]);
+			b[3] = ZFrustum::GetInstance()->IsIn(&g_pvHeightMap[i[3]]);
+			if (b[2] | b[1] | b[3])
+			{
+				idx._0 = i[2];
+				idx._1 = i[1];
+				idx._2 = i[3];
+				*pI++ = idx;
+				g_nTriangles++;
+			}
+		}
+	}
+
+	g_pIB->Unlock();
+	return S_OK;
+}
+
 void ProcessMouse()
 {
 	POINT pt;
@@ -295,80 +356,22 @@ void ProcessInputs(void)
 	ProcessKey();
 }
 
-VOID SetupLights()
-{
-	D3DMATERIAL9 mtrl;
-	ZeroMemory(&mtrl, sizeof(D3DMATERIAL9));
-	mtrl.Diffuse.r = mtrl.Ambient.r = 1.0f;
-	mtrl.Diffuse.g = mtrl.Ambient.g = 1.0f;
-	mtrl.Diffuse.b = mtrl.Ambient.b = 1.0f;
-	mtrl.Diffuse.a = mtrl.Ambient.a = 1.0f;
-	g_pd3dDevice->SetMaterial(&mtrl);
-
-	D3DXVECTOR3 vecDir;
-	D3DLIGHT9 light;
-	ZeroMemory(&light, sizeof(D3DLIGHT9));
-	light.Type = D3DLIGHT_DIRECTIONAL;
-	light.Diffuse.r = 1.0f;
-	light.Diffuse.g = 1.0f;
-	light.Diffuse.b = 0.0f;
-	//vecDir = D3DXVECTOR3(1, 1, 1);
-	vecDir = D3DXVECTOR3(cosf(GetTickCount() / 350.0f), 1.0f, sinf(GetTickCount() / 350.0f));
-	D3DXVec3Normalize((D3DXVECTOR3*)&light.Direction, &vecDir);
-	light.Range = 1000.0f;
-
-	g_pd3dDevice->SetLight(0, &light);
-	g_pd3dDevice->LightEnable(0, TRUE);
-	g_pd3dDevice->SetRenderState(D3DRS_LIGHTING, TRUE);
-	g_pd3dDevice->SetRenderState(D3DRS_AMBIENT, 0x00909090);
-}
-
-void LogFPS(void)
-{
-	static DWORD nTick = 0;
-	static DWORD nFPS = 0;
-
-	if (GetTickCount() - nTick > 1000)
-	{
-		nTick = GetTickCount();
-		ZFLog::GetInstance()->Log("FPS : %d", nFPS);
-		nFPS = 0;
-
-		return;
-	}
-
-	nFPS++;
-}
-
 VOID Animate()
 {
-	/// 0 ~ 2PI 까지(0~360도) 값을 변화시킴 Fixed Point기법 사용
-	DWORD d = GetTickCount() % ((int)((D3DX_PI * 2) * 1000));
-	/// Y축 회전행렬
-	//	D3DXMatrixRotationY( &g_matAni, d / 1000.0f );
 	D3DXMatrixIdentity(&g_matAni);
-	D3DXMatrixIdentity(&g_matAni2);
-	auto mat = *ZCamera::GetInstance()->GetBillMatrix();
-	
-	mat._22 = 1;
-	mat._23 = 1;
-	mat._32 = 1;
-	mat._33 = 1;
-
-	g_matAni2 = g_matAni2 * mat;
-
-	//g_matAni2._11 = ZCamera::GetInstance()->GetBillMatrix()->_11;
-	//g_matAni2._13 = ZCamera::GetInstance()->GetBillMatrix()->_13;
-	//g_matAni2._31 = ZCamera::GetInstance()->GetBillMatrix()->_31;
-	//g_matAni2._33 = ZCamera::GetInstance()->GetBillMatrix()->_33;
-	//D3DXMatrixInverse(&g_matAni2, NULL, &g_matAni2);
-
-
-
-	LogFPS();
 
 	SetupLights();
 	ProcessInputs();
+
+	D3DXMATRIXA16 m;
+	D3DXMATRIXA16 *pView;
+	pView = ZCamera::GetInstance()->GetViewMatrix();
+	m = *pView * g_matProj;
+	if (!g_bLockFrustum)
+		ZFrustum::GetInstance()->Make(&m);
+	ProcessFrustumCull();
+
+	LogFPS();
 }
 
 void CleanUp()
@@ -383,21 +386,13 @@ void CleanUp()
 
 void DrawMesh(D3DXMATRIXA16* pMat)
 {
+	g_pd3dDevice->SetRenderState(D3DRS_FILLMODE, g_bWireframe ? D3DFILL_WIREFRAME : D3DFILL_SOLID);
+
 	g_pd3dDevice->SetTransform(D3DTS_WORLD, pMat);
 	g_pd3dDevice->SetStreamSource(0, g_pVB, 0, sizeof(CUSTOMVERTEX));
-	g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
+	g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVVERTEX);
 	g_pd3dDevice->SetIndices(g_pIB);
-	g_pd3dDevice->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, g_cxHeight * g_czHeight, 0,
-		(g_cxHeight - 1) * (g_czHeight - 1) * 2);
-}
-
-void DrawMesh2(D3DXMATRIXA16* pMat)
-{
-	g_pd3dDevice->SetTransform(D3DTS_WORLD, pMat);
-	g_pd3dDevice->SetStreamSource(0, g_pVB2, 0, sizeof(CUSTOMVERTEX2));
-	g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX2);
-	g_pd3dDevice->SetIndices(g_pIB2);
-	g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
+	g_pd3dDevice->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, g_cxHeight*g_czHeight, 0, g_nTriangles);
 }
 
 void Render()
@@ -408,20 +403,6 @@ void Render()
 
 	if (SUCCEEDED(g_pd3dDevice->BeginScene()))
 	{
-		g_pd3dDevice->SetTexture(0, g_pTexDiffuse2);
-		g_pd3dDevice->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
-		g_pd3dDevice->SetTextureStageState(0, D3DTSS_TEXCOORDINDEX, 0);
-		g_pd3dDevice->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE);
-		g_pd3dDevice->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
-		g_pd3dDevice->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_DIFFUSE);
-
-		g_pd3dDevice->SetTransform(D3DTS_WORLD, &g_matAni2);
-		g_pd3dDevice->SetStreamSource(0, g_pVB2, 0, sizeof(CUSTOMVERTEX2)); // 정점세팅
-		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX2);
-		//g_pd3dDevice->SetIndices(g_pIB2);
-		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
-		
-
 		g_pd3dDevice->SetTexture(0, g_pTexDiffuse);
 		g_pd3dDevice->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
 		g_pd3dDevice->SetTextureStageState(0, D3DTSS_TEXCOORDINDEX, 0);
@@ -430,6 +411,9 @@ void Render()
 		g_pd3dDevice->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_DIFFUSE);
 
 		DrawMesh(&g_matAni);
+
+		if (!g_bHideFrustum)
+			ZFrustum::GetInstance()->Draw(g_pd3dDevice);
 
 		g_pd3dDevice->EndScene();
 	}
@@ -448,8 +432,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 		PostQuitMessage(0);
 		return 0;
 	case WM_KEYDOWN:
-		if (wParam == VK_ESCAPE)
+		switch (wParam)
+		{
+		case VK_ESCAPE:
 			PostMessage(hWnd, WM_DESTROY, 0, 0);
+			break;
+		case '1':
+			g_bWireframe = !g_bWireframe;
+			break;
+		case '2':
+			g_bLockFrustum = !g_bLockFrustum;
+			g_bHideFrustum = !g_bHideFrustum;
+			break;
+		}
+
+
+
 		return 0;
 	}
 
@@ -504,6 +502,8 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	}
 
 	ZFLog::Release();
+	ZCamera::Release();
+	ZFrustum::Release();
 
 	UnregisterClass(g_szClassName, hInstance);
 
